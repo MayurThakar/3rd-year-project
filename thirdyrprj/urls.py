a2e63160b0eb019a0f834cc133d49cba2fbcddf6
MayurThakar <mayur.prohgramer@gmail.com>
2021-06-26 18:45:59 +0530

    added hod page

--- thirdyrprj/urls.py
+++ thirdyrprj/urls.py
@@ -22,6 +22,6 @@ from webapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
-    # path('', views.hod_page, name='hod_page'),
+    path('hod', views.hod_page, name='hod_page'),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
